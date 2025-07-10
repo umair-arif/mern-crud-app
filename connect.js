b3479bd3a331a94b3d56df86e5c672ea4cdcd12b
umair-arif <umair.aarifs@gmail.com>
2025-07-11 01:02:01 +0500

    make some change

--- connect.js
+++ connect.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 async function connectToMongoDb(url) {
-  return mongoose.connect(url);
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 }
 
 module.exports = { connectToMongoDb };
